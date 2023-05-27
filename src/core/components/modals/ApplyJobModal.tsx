fcdcd83f768d0e413c9a17ded15c7f21d287e6db
Phạm Vĩnh Tài <kainesv86@gmail.com>
2023-05-27 15:39:38 +0700

    Fix job

--- src/core/components/modals/ApplyJobModal.tsx
+++ src/core/components/modals/ApplyJobModal.tsx
@@ -76,9 +76,9 @@ const ApplyJobModal: React.FunctionComponent<ApplyJobModalProps> = () => {
                                     {...register('jobId', { required: true })}
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                                 >
-                                    <option>United States</option>
-                                    <option>Canada</option>
-                                    <option>Mexico</option>
+                                    <option>Marketing</option>
+                                    <option>Software Engineer</option>
+                                    <option>Business Analyst</option>
                                 </select>
                             </div>
                         </div>
